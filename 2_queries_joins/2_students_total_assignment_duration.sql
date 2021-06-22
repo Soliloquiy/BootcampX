63c53570bc0c310130d30eae9f1cce945586bf01
Soliloquy <ibrahia@ucalgary.ca>
2021-06-22 00:48:03 +0000

    Add 3_queries_group-by folder with relative queries. Edit all files to end in ;.

--- 2_queries_joins/2_students_total_assignment_duration.sql
+++ 2_queries_joins/2_students_total_assignment_duration.sql
@@ -2,4 +2,4 @@ SELECT SUM(duration) as total_duration
 FROM assignment_submissions
 JOIN students
 ON student_id = students.id
-WHERE students.name IN ('Ibrahim Schimmel')
+WHERE students.name IN ('Ibrahim Schimmel');
